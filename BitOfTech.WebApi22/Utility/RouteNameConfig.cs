09b5c56d62db9de97075e71476be743cd7c31ae6
Tran Thuong Thien <tranthiencdsp@gmail.com>
2016-10-09 14:36:23 +0700

    Add RouteNameConfig

---
+++ BitOfTech.WebApi22/Utility/RouteNameConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace BitOfTech.WebApi22.Utility
+{
+    public class RouteNameConfig
+    {
+        public const string API_ROLE = "api/roles";
+        public const string API_USER = "api/accounts";
+
+        public const string ROUTE_TEMPLATE_ID = "{id}";
+        public const string ROUTE_TEMPLATE_ALL = "all";
+        public const string ROUTE_TEMPLATE_CREATE = "create";
+
+        public const string ROUTE_GET_ROLE_BY_ID = "GetRoleById";
+        public const string ROUTE_GET_ALL_ROLES = "GetAll";
+    }
+}
